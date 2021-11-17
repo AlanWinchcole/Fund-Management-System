0eae55db256f84d19d2472996fe96055226f33c2
Lewis William Lunny <2397246l@student.gla.ac.uk>
2021-11-17 19:13:25 +0000

    Created UserProfile model, fixed typo in settings.py line 87 os.path.joint to os.path.join, added UserProfile model to admin.py,migrated model to database

--- fund_management_website/fund/models.py
+++ fund_management_website/fund/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserProfile(models.Model):
+	userID = models.AutoField(primary_key=True)
+	user = models.OneToOneField(User,on_delete=models.DO_NOTHING)
+	verified = models.BooleanField(default = False)
+	userType = models.CharField(max_length = 50)
+	def __str__(self):
+		return self.user.username
