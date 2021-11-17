e7237b2a7030082712ca4ae3027658eea91e6bcf
Lewis William Lunny <2397246l@student.gla.ac.uk>
2021-11-17 22:43:37 +0000

    Created register view in views.py

--- fund_management_website/fund/views.py
+++ fund_management_website/fund/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+#from fund.forms import RegisterForm
 # Create your views here.
+def register(request):
+	registered = False
+
+	if request.method == 'POST':
+		register_form = RegisterForm(data=request.POST)
+		if register_form.is_valid():
+			user = register_form.save()
+			user.set_password(user.password)
+			user.save()
+			registered = True
+		else:
+			print(register_form.errors)
+	else:
+		register_form = RegisterForm()
+	return render(request,
+		'fund/register.html',
+		{'register_form':register_form,
+		'registered':registered})
