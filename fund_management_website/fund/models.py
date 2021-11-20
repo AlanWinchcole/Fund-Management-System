55e93cc795277e26293acd49f501292d24a0d6e0
JosephMousa_2453578m <2453578m@student.gla.ac.uk>
2021-11-20 23:45:40 +0000

    Application Model

--- fund_management_website/fund/models.py
+++ fund_management_website/fund/models.py
@@ -9,3 +9,28 @@ class UserProfile(models.Model):
 	userType = models.CharField(max_length = 50)
 	def __str__(self):
 		return self.user.username
+
+class ApplicationData(models.Model):
+        
+        applicationID = models.AutoField(primary_key=True, unique=True)
+        organisationName = models.CharField(max_length=200)
+        projectTitle = models.CharField(max_length=200)
+        CH_OSCR_number = models.CharField(max_length=20, unique=True, blank=True)
+        contactName = models.CharField(max_length=200)
+        contactEmail = models.EmailField(max_length=254)
+        projectDesc = models.CharField(max_length=300)
+        userGroupDesc = models.CharField(max_length=300)
+        learningOpp = models.CharField(max_length=300)
+        keyPartnersWork = models.CharField(max_length=300)
+        projImpactClimate = models.CharField(max_length=300)
+        projSupportLocBus = models.CharField(max_length=300)
+        proContribution = models.CharField(max_length=300)
+        feedback = models.CharField(max_length=500)       
+        
+
+        #need to create a class for budget profile and link to app data
+
+#class BudgetProfile(models.Model):
+        
+        
+        
