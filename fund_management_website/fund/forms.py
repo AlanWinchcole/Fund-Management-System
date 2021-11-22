add33a209dbab69711f7075e0a4ea52202126252
Yogesh Parajuli <2569240p@student.gla.ac.uk>
2021-11-22 14:22:51 +0000

    Created form.py and added forms to associate to UserProfile model

---
+++ fund_management_website/fund/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from django.forms import Modelform
+from .models import *
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+        labels = {'username': "Username",
+                    'email': "Email Address",
+                    'password': 'Password', }
+
+
+)
+class Form(forms.ModelForm):
+    userType = forms.ChoiceField()
+    class Meta:
+        model=UserProfile
+        fields=('verified',)
+
+        labels = {
+            'verified': "Is the user verified",
+            'userType': "Type of user"
+        }
+        widgets = {
+            'userType': forms.RadioSelect(choices = [(1, "LAG Member"), (2,"Volunteer"), (3, "Applicant"), (4, "Other")])
+        }
+
+)
