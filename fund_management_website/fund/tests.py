4519fa48e8120c0f23cb3fa571689c8e1c0fcc74
JosephMousa_2453578m <2453578m@student.gla.ac.uk>
2022-01-18 13:26:45 +0200

    Unit tests, database issues resolved

--- fund_management_website/fund/tests.py
+++ fund_management_website/fund/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+#Testing Views
+
+#Test suite for Index page
+class IndexViewTest(TestCase):
+
+    def test_index_view_load(self):
+
+        response = self.client.get('http://127.0.0.1:8000/welcome/')
+        self.assertEqual(response.status_code, 200)
+
+#Test suite for Register page
+class RegisterViewTest(TestCase):
+
+    def test_register_view_load(self):
+
+        response = self.client.get('http://127.0.0.1:8000/fund/register/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_register_template_used(self):
+
+        response = self.client.get('http://127.0.0.1:8000/')
+        self.assertTemplateUsed(response, 'fund/register.html') 
