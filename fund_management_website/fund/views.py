fdacb80a4acd36a4132efcc73bc74bda42ba8c10
JosephMousa_2453578m <2453578m@student.gla.ac.uk>
2022-03-21 22:34:10 +0200

    view status view

--- fund_management_website/fund/views.py
+++ fund_management_website/fund/views.py
@@ -410,8 +410,10 @@ def view_application_status(request, id):
     print(comments)
     for comment in comments:
         print(comment.comment)
-    else:
+    else if application.reviewed:
         return render(request, 'fund/application_view.html', {'application':application, 'application_form' :application_form, 'comments':comments, 'admin':admin, 'review':review})
+    else:
+        return render(request, 'fund/application_view.html', {'application':application, 'application_form' :application_form, 'comments':comments, 'admin':admin,})
 
 
 def add_comment(request, id):
